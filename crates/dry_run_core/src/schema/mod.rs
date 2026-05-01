6f7d0fae1d7fe4de8893cb707ed39e93acc59639
Radim Marek <radim@boringsql.com>
2026-05-02 01:23:13 +0200

    feat: planner_stats / activity_stats capture

--- crates/dry_run_core/src/schema/mod.rs
+++ crates/dry_run_core/src/schema/mod.rs
@@ -8,6 +8,9 @@ pub mod vacuum;
 
 pub use hash::{HashInput, compute_content_hash};
 pub use inject::{ApplyResult, apply_stats};
-pub use introspect::{fetch_is_standby, fetch_stats_only, introspect_schema};
+pub use introspect::{
+    fetch_is_standby, fetch_stats_only, introspect_activity_stats, introspect_planner_stats,
+    introspect_schema,
+};
 pub use profile::*;
 pub use types::*;
