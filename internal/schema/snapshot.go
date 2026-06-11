8b3ffd456f092834ccf9877a57c10d46d13d9bcf
Radim Marek <radim@boringsql.com>
2026-06-11 21:45:40 +0200

    fix: missing file

---
+++ internal/schema/snapshot.go
@@ -0,0 +1,74 @@
+package schema
+
+import "github.com/boringsql/dryrun/pkg/snapshot"
+
+// Snapshot types.
+type (
+	SchemaSnapshot        = snapshot.SchemaSnapshot
+	Table                 = snapshot.Table
+	Column                = snapshot.Column
+	Constraint            = snapshot.Constraint
+	ConstraintKind        = snapshot.ConstraintKind
+	Index                 = snapshot.Index
+	ColumnStats           = snapshot.ColumnStats
+	PartitionInfo         = snapshot.PartitionInfo
+	PartitionStrategy     = snapshot.PartitionStrategy
+	PartitionChild        = snapshot.PartitionChild
+	RlsPolicy             = snapshot.RlsPolicy
+	Trigger               = snapshot.Trigger
+	EnumType              = snapshot.EnumType
+	DomainType            = snapshot.DomainType
+	CompositeType         = snapshot.CompositeType
+	CompositeField        = snapshot.CompositeField
+	View                  = snapshot.View
+	Function              = snapshot.Function
+	Volatility            = snapshot.Volatility
+	Extension             = snapshot.Extension
+	GucSetting            = snapshot.GucSetting
+	StaleStatsEntry       = snapshot.StaleStatsEntry
+	QualifiedName         = snapshot.QualifiedName
+	TableSizing           = snapshot.TableSizing
+	TableActivity         = snapshot.TableActivity
+	IndexSizing           = snapshot.IndexSizing
+	IndexActivity         = snapshot.IndexActivity
+	NodeIdentity          = snapshot.NodeIdentity
+	TableSizingEntry      = snapshot.TableSizingEntry
+	IndexSizingEntry      = snapshot.IndexSizingEntry
+	ColumnStatsEntry      = snapshot.ColumnStatsEntry
+	TableActivityEntry    = snapshot.TableActivityEntry
+	IndexActivityEntry    = snapshot.IndexActivityEntry
+	PlannerStatsSnapshot  = snapshot.PlannerStatsSnapshot
+	ActivityStatsSnapshot = snapshot.ActivityStatsSnapshot
+	AnnotatedSchema       = snapshot.AnnotatedSchema
+	MergedActivity        = snapshot.MergedActivity
+	NodeActivity          = snapshot.NodeActivity
+)
+
+// Enum values.
+const (
+	ConstraintPrimaryKey = snapshot.ConstraintPrimaryKey
+	ConstraintForeignKey = snapshot.ConstraintForeignKey
+	ConstraintUnique     = snapshot.ConstraintUnique
+	ConstraintCheck      = snapshot.ConstraintCheck
+	ConstraintExclusion  = snapshot.ConstraintExclusion
+
+	PartitionRange = snapshot.PartitionRange
+	PartitionList  = snapshot.PartitionList
+	PartitionHash  = snapshot.PartitionHash
+
+	VolatilityImmutable = snapshot.VolatilityImmutable
+	VolatilityStable    = snapshot.VolatilityStable
+	VolatilityVolatile  = snapshot.VolatilityVolatile
+)
+
+// Functions.
+var (
+	ConstraintKindFromPg    = snapshot.ConstraintKindFromPg
+	PartitionStrategyFromPg = snapshot.PartitionStrategyFromPg
+	VolatilityFromPg        = snapshot.VolatilityFromPg
+	DetectStaleStats        = snapshot.DetectStaleStats
+
+	ComputeContentHash         = snapshot.ComputeContentHash
+	ComputePlannerContentHash  = snapshot.ComputePlannerContentHash
+	ComputeActivityContentHash = snapshot.ComputeActivityContentHash
+)
